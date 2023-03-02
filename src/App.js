96a4ece914f2c3ef04d8fff16cecd634208c2d07
Yogi Adam Firdaus <yogiadamf@gmail.com>
2023-03-02 07:25:21 +0700

    add setup components and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import React from 'react'
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import "./App.css";
 
 const App = () => {
+  const activeMenu = true
   return (
-    <h1 className="underline">App</h1>
-  )
-}
+    <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <TooltipComponent content="Settings" position="Top">
+            <button
+              type="button"
+              className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{ background: 'blue', borderRadius: '50%'}}
+            >
+              <FiSettings />
+            </button>
+          </TooltipComponent>
+        </div>
+        {activeMenu ? (
+          <div className="w-72 fixed sidebar 
+          dark:bg-secondary-dark-bg 
+          bg-white">
+            Sidebar
+          </div>
+        ):(
+          <div className="w-0 dark:bg-secondary-dark-bg">
+            Sidebar w-0
+          </div>
+        )}
+        <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72': 'flex-2'}`}>
+          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+            Navbar
+          </div>
+        </div>
 
-export default App
+        <div>
+          <Routes>
+            <Route path="/" element="Ecommerce"/>
+            <Route path="/ecommerce" element="Ecommerce"/>
+
+            <Route path="/orders" element="Orders"/>
+            <Route path="/employees" element="employees"/>
+            <Route path="/customers" element="customers"/>
+
+            <Route path="/kanban" element="kanban"/>
+            <Route path="/editor" element="editor"/>
+            <Route path="/calendar" element="calendar"/>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
